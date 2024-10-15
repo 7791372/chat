b5da7349803765374066a8361b79050bb5a37691
Your <184955262+7791372@users.noreply.github.com>
2024-10-15 16:16:28 +0900

    stuff

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+// use tokio::net::TcpStream;
+
+#[tokio::main]
+async fn main() {
+    // clear the entire screen
+    print!("\x1B[2J\x1B[3J\x1B[H");
 }
