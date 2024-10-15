6952c89f5faacbadd8be8b56cd3504919ede54b6
Your <184955262+7791372@users.noreply.github.com>
2024-10-15 18:11:47 +0900

    add read message functionality

---
+++ client/src/read_message.rs
@@ -0,0 +1,14 @@
+use tokio::io::{AsyncReadExt, BufReader};
+use tokio::net::tcp::OwnedReadHalf;
+
+pub async fn read_message(reader: OwnedReadHalf) {
+    let mut buf_reader = BufReader::new(reader);
+    let mut buffer = vec![0; 512];
+    while let Ok(n) = buf_reader.read(&mut buffer).await {
+        if n == 0 {
+            break;
+        }
+        let msg = String::from_utf8_lossy(&buffer[..n]);
+        print!("{}", msg)
+    }
+}
