35e45caa35d2600941b7efbcbe0ecd758c8a1793
Your <184955262+7791372@users.noreply.github.com>
2024-10-15 16:52:47 +0900

    aaa idk

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,7 +1,17 @@
 // use tokio::net::TcpStream;
 
+mod ui;
+
 #[tokio::main]
 async fn main() {
     // clear the entire screen
     print!("\x1B[2J\x1B[3J\x1B[H");
+
+    // TODO: add port option in .config/chat
+    // let stream = TcpStream::connect("127.0.0.1:11945").await.expect("Failed to connect");
+    // let (reader, writer) = stream.into_split();
+
+    ui::header::draw();
+    ui::body::draw();
+    ui::footer::draw();
 }
