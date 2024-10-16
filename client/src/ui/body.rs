fa61ea12b9b7c4fb5832d7d33ef3d573e13d3e85
Your <184955262+7791372@users.noreply.github.com>
2024-10-16 17:31:19 +0900

    remove everything

--- client/src/ui/body.rs
+++ client/src/ui/body.rs
@@ -1,26 +1,22 @@
-use std::io::{self, Write};
-use crossterm::terminal::size;
+// use std::io::{self, Write};
+// use crossterm::terminal::size;
 
-pub fn draw() {
-    let (width, height) = size().unwrap();
+// pub fn draw() {
+//     let (width, height) = size().unwrap();
 
-    // draw the chat lines
-    if height > 2 {
-        for row in 3..height-2 {
-            print!("\x1B[{};1H~", row);
-        }
-    }
+//     // draw the chat lines
+//     if height > 2 {
+//         for row in 3..height-2 {
+//             print!("\x1B[{};1H~", row);
+//         }
+//     }
 
-    // draw the sidebar if terminal is wide enough
-    if width > 69 {
-        for row in 1..height+1 {
-            print!("\x1B[{};{}H┃", row, width-21);
-        }
-    }
+//     // draw the sidebar if terminal is wide enough
+//     if width > 69 {
+//         for row in 1..height+1 {
+//             print!("\x1B[{};{}H┃", row, width-21);
+//         }
+//     }
 
-    // tests
-    print!("\x1B[10;1H250115 00:00");
-    print!("\x1B[11;1H[yuna@oldinternet] xd");
-
-    io::stdout().flush().unwrap();
-}
+//     io::stdout().flush().unwrap();
+// }
