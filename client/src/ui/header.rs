2e0ebf84d9141db14032b75bad6784a65a84e145
Your <184955262+7791372@users.noreply.github.com>
2024-10-17 22:10:02 +0900

    fix title length

--- client/src/ui/header.rs
+++ client/src/ui/header.rs
@@ -5,6 +5,6 @@ pub fn draw() {
         let (width, _height) = size().unwrap();
 
         // Move cursor to top of screen and print header
-        print!("\x1B[1;{}Hkonata@chat\x1B[0m", (width - 5) / 2);
+        print!("\x1B[1;{}Hkonata@chat\x1B[0m", (width - 11) / 2);
         io::stdout().flush().unwrap();
 }
