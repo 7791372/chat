17222d711dae6fcb9e06cb0d569fb16f20941d7b
Your <184955262+7791372@users.noreply.github.com>
2024-10-19 01:08:21 +0900

    clean up

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,29 +1,30 @@
-// use tokio::net::TcpStream;
+use tokio::net::TcpStream;
+use crossterm::terminal;
 
 mod read_message;
 mod write_message;
+mod input_handler;
 mod ui;
 
 #[tokio::main]
 async fn main() {
-    // clear the entire screen
+    terminal::enable_raw_mode().unwrap();
     print!("\x1B[2J\x1B[3J\x1B[H");
 
     // TODO: add port option in .config/chat
-    // let stream = TcpStream::connect("127.0.0.1:11945").await.expect("Failed to connect");
-    // let (reader, writer) = stream.into_split();
+    let stream = TcpStream::connect("127.0.0.1:11945").await.expect("Failed to connect");
+    let (reader, writer) = stream.into_split();
 
     ui::header::draw();
-    ui::body::draw();
     ui::footer::draw();
 
-    // let read_task = tokio::spawn(async move {
-    //     read_message::read_message(reader).await;
-    // });
+    let read_task = tokio::spawn(async move {
+        read_message::read_message(reader).await;
+    });
 
-    // let write_task = tokio::spawn(async move {
-    //     write_message::write_message(writer).await;
-    // });
+    let write_task = tokio::spawn(async move {
+        input_handler::handle_input(writer).await;
+    });
 
-    // let _ = tokio::join!(read_task, write_task);
+    let _ = tokio::join!(read_task, write_task);
 }
