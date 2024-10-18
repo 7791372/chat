adbdd1c5a6d3836f622d64758d7179209909ebb0
Your <184955262+7791372@users.noreply.github.com>
2024-10-19 01:54:17 +0900

    umm idk

--- client/src/read_message.rs
+++ client/src/read_message.rs
@@ -1,13 +1,16 @@
+// Reads a message from the server
+
 use tokio::io::{AsyncReadExt, BufReader};
 use tokio::net::tcp::OwnedReadHalf;
 
 pub async fn read_message(reader: OwnedReadHalf) {
     let mut buf_reader = BufReader::new(reader);
     let mut buffer = vec![0; 512];
+
     while let Ok(n) = buf_reader.read(&mut buffer).await {
-        if n == 0 {
-            break;
-        }
+        // server probably closed if this is true
+        if n == 0 { break };
+
         let msg = String::from_utf8_lossy(&buffer[..n]);
         print!("{}", msg)
     }
