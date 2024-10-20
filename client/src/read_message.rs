0f519acec7c846fa0061472e1528c4acac215f69
Your <184955262+7791372@users.noreply.github.com>
2024-10-20 21:02:46 +0900

    yes

--- client/src/read_message.rs
+++ client/src/read_message.rs
@@ -2,16 +2,30 @@
 
 use tokio::io::{AsyncReadExt, BufReader};
 use tokio::net::tcp::OwnedReadHalf;
+use std::io::{self, Write};
 
 pub async fn read_message(reader: OwnedReadHalf) {
     let mut buf_reader = BufReader::new(reader);
     let mut buffer = vec![0; 512];
 
     while let Ok(n) = buf_reader.read(&mut buffer).await {
-        // server probably closed if this is true
-        if n == 0 { break };
+        if n == 0 {
+            eprintln!("Connection closed");
+            break;
+         }
 
-        let msg = String::from_utf8_lossy(&buffer[..n]);
-        print!("\r{}", msg)
+        let msg = String::from_utf8_lossy(&buffer[..n]).to_string();
+        if let Some((name, content)) = parse_message(&msg) {
+            println!("\r\x1B[2K{}: {}", name, content);
+            io::stdout().flush().unwrap();
+        }
     }
+}
+
+fn parse_message(msg: &str) -> Option<(&str, &str)> {
+    let parts: Vec<&str> = msg.splitn(2, ':').collect();
+    if parts.len() == 2 {
+        return Some((parts[0], parts[1]));
+    }
+    None
 }
