85824b0fa0d94b85eddcde00718459a9800e2fc1
Your <184955262+7791372@users.noreply.github.com>
2024-10-20 21:03:24 +0900

    add message format

--- client/src/write_message.rs
+++ client/src/write_message.rs
@@ -4,5 +4,10 @@ use tokio::io::AsyncWriteExt;
 use tokio::net::tcp::OwnedWriteHalf;
 
 pub async fn write_message(writer: &mut OwnedWriteHalf, message: String) {
-    let _ = writer.write_all(message.as_bytes()).await;
+    let name = "anon";
+    let formatted_message = format!("{}:{}", name, message);
+
+    if let Err(e) = writer.write_all(formatted_message.as_bytes()).await {
+        eprintln!("Failed to write message: {}", e); 
+    }
 }
